0dee05fa62b400387e418770a3980b80808d8aea
Ayushi <ayushik882525@gmail.com>
2026-01-17 11:46:05 +0530

    First commit

---
+++ intro.js
@@ -0,0 +1,3 @@
+var user = { name: "ayushi", age: 18 };
+console.log(user.age);
+console.log(user.name);
---
+++ intro.ts
@@ -0,0 +1,3 @@
+let user ={name:"ayushi",age: 18}
+console.log(user.age);
+console.log(user.name);
