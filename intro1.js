d255f6469975dab91efa1ffac44eb952b016c76b
Ayushi <ayushik882525@gmail.com>
2026-02-10 08:02:25 +0530

    Remove tracked JavaScript files and ignore JS builds

--- intro1.js
+++
@@ -1,5 +0,0 @@
-// let user ={name:"ayushi",age: 18}
-// console.log(user.age);
-// console.log(user.name);
-var message = "Hello world";
-console.log(message);
--- intro5.js
+++
@@ -1,8 +0,0 @@
-var answers = {};
-function recordAnswer(questionId, answer) {
-    answers[questionId] = answer;
-}
-recordAnswer("q1", "yes");
-recordAnswer("q2", 54);
-recordAnswer("q3", true);
-console.log(answers);
